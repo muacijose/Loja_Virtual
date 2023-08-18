bc9276e3c463722f3cd924bdf21381c6f0b5eab8
muacijose <muaci@outlook.com>
2023-08-18 20:33:41 -0300

    commit inicial do projeto

---
+++ loja_virtual/src/test/java/muaci/dev/lojavirtual/loja_virtual/LojaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package muaci.dev.lojavirtual.loja_virtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
