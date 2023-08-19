50b64f7ae391d66550f6d277ef8cdd96898e7e9e
muacijose <muaci@outlook.com>
2023-08-19 06:20:04 -0300

    criando a conxão com o banco de dados postgressql e tabela produto

---
+++ loja_virtual/src/main/java/muaci/dev/lojavirtual/loja_virtual/model/MarcaProduto.java
@@ -0,0 +1,31 @@
+package muaci.dev.lojavirtual.loja_virtual.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+    @Entity
+    @Table(name = "marca_produto")
+    @SequenceGenerator(name = "marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+    public class MarcaProduto implements Serializable {
+        private static final long serialVersionUID = 1L;
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "seq_marca_produto")
+        private Long id;
+        private String nomeDesc;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getNomeDesc() {
+            return nomeDesc;
+        }
+
+        public void setNomeDesc(String nomeDesc) {
+            this.nomeDesc = nomeDesc;
+        }
+}
