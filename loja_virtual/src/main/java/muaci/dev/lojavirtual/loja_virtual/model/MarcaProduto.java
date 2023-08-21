04ca647450f386c4f2fc3bd09a64f130b04d0691
muacijose <muaci@outlook.com>
2023-08-21 05:45:09 -0300

    modificado

--- loja_virtual/src/main/java/muaci/dev/lojavirtual/loja_virtual/model/MarcaProduto.java
+++ loja_virtual/src/main/java/muaci/dev/lojavirtual/loja_virtual/model/MarcaProduto.java
@@ -6,9 +6,9 @@ import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
-    @Table(name = "marca_produto")
-    @SequenceGenerator(name = "marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
-    public class MarcaProduto implements Serializable {
+@Table(name = "marca_produto")
+@SequenceGenerator(name = "marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+public class MarcaProduto implements Serializable {
         private static final long serialVersionUID = 1L;
         @Id
         @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "seq_marca_produto")
