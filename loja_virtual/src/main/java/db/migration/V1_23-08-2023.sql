dad2ae4ad7fae73068101b8851bac0a9273213e3
muacijose <muaci@outlook.com>
2023-08-23 06:20:02 -0300

    Criado o db_migration

---
+++ loja_virtual/src/main/java/db/migration/V1_23-08-2023.sql
@@ -0,0 +1,4 @@
+Select constraint_name from information_schema.constraint_column_usage
+where table_name = 'usuario_acesso' and column_name = 'acesso_id'
+and constraint_name <> 'unique_acesso_user';
+alter table usuario_acesso DROP CONSTRAINT
