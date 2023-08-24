f108884842feca38a26908d169d761854d07e5d3
muacijose <muaci@outlook.com>
2023-08-24 19:28:36 -0300

    Criado classe formulario pagamento

---
+++ loja_virtual/src/main/java/muaci/dev/lojavirtual/loja_virtual/model/FormaPagamento.java
@@ -0,0 +1,46 @@
+package muaci.dev.lojavirtual.loja_virtual.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "forma_pagamento")
+@SequenceGenerator(name = "seq_forma_pagamento", sequenceName = "seq_forma_pagamento",allocationSize = 1, initialValue = 1)
+public class FormaPagamento implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_forma_pagamento")
+    private Long id;
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FormaPagamento that)) return false;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
